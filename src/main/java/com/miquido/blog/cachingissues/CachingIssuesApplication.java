c8a8b6932be8b36945b37b7c4ab7d087b6dba16f
Mateusz Kuźmik <mateusz.kuzmik1@gmail.com>
2019-11-19 19:21:52 +0100

    Initial commit - Application template, H2 inmemory database

---
+++ src/main/java/com/miquido/blog/cachingissues/CachingIssuesApplication.java
@@ -0,0 +1,13 @@
+package com.miquido.blog.cachingissues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachingIssuesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CachingIssuesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miquido/blog/cachingissues/CachingIssuesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miquido.blog.cachingissues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachingIssuesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
